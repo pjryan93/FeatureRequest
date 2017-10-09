f2230b72415b96b16b8b74b6623e3c7b9a10e57f
Patrick Ryan <pjryan@my.okcu.edu>
2017-10-09 14:14:53 -0500

    fixed typos

--- web/create_db.py
+++ web/create_db.py
@@ -5,7 +5,7 @@ source_dir = os.path.abspath(os.curdir)
 destination_dir = os.path.join(source_dir, '../postgresql')
 if not os.path.isdir(destination_dir):
 	os.makedirs(destination_dir)
-with open(os.path.join(destination_dir, docker_file), 'w') as postgres_dockerfile:
+with open(os.path.join(destination_dir, Dockerfile), 'w') as postgres_dockerfile:
    postgres_dockerfile.write('FROM postgres:9.6')
    postgres_dockerfile.write('\n')
    postgres_dockerfile.write('\n# Set environment variables')
